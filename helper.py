ba203f1a827d03421d2b836b18f39e8a74a542bb
Tommaso Crippa <crippa.tommaso@gmail.com>
2022-02-15 16:30:40 +0100

    Commented code

--- helper.py
+++ helper.py
@@ -16,43 +16,34 @@ original = words_list
 
 clues = []
 
-root = Tk()
+root = Tk() # Creating main window
 root.iconbitmap("materials/duh.ico")
 root.title("Wordle Helper")
 root.resizable(False, False)
 
 
 image = ImageTk.PhotoImage(Image.open("materials/helper.png"))
-
-title = Label(root, image=image)
+title = Label(root, image=image) # Adding title
 title.grid(row=0, column=0, columnspan=4, sticky=N+S+W+E)
 
-def character_limit(entry_text):
-    digits = []
-    for i in range(10):
-        digits.append(str(i))
-    
-    if entry_text.get() in digits:
-        print("hi")
-
+def character_limit(entry_text): # Used to get only one letter
     if len(entry_text.get()) > 0:
         entry_text.set(entry_text.get()[-1])
 
-def isalpha(letter):
+def isalpha(letter): # Used to get only letters
     alpha = 'abcdefghijklmnopqrstuvwxyz'
     return letter in alpha
-
 validation = root.register(isalpha)
 
-letter = StringVar() # the text in  your entry
+letter = StringVar() # Letter insertion widgets
 letter.set(" ")
-letter_widget = Entry(root, textvariable = letter, validate='all', validatecommand=(validation, '%S'), width=3) # the entry
+letter_widget = Entry(root, textvariable = letter, validate='all', validatecommand=(validation, '%S'), width=3) 
 letter_widget.grid(row=2, column=0, rowspan=2)
 letter_label = Label(root, text="LETTER:")
 letter_label.grid(row=1, column=0) 
 letter.trace("w", lambda *args: character_limit(letter))
 
-type = IntVar()
+type = IntVar() # Type insertion widgets
 type.set(0)
 isnotin_widget = Radiobutton(root, variable=type, value=0, text="Is not in word", bg="gray")
 isnotin_widget.grid(row=1, column=1, columnspan=2, sticky=N+S+W+E)
@@ -61,14 +52,14 @@ isin_widget.grid(row=2, column=1, columnspan=2, sticky=N+S+W+E)
 iscorrect_widget = Radiobutton(root, variable=type, value=2, text="Is placed correctly", bg="green")
 iscorrect_widget.grid(row=3, column=1, columnspan=2, sticky=N+S+W+E)
 
-root.bind("<exclam>", lambda event: type.set(0))
+root.bind("<exclam>", lambda event: type.set(0))  # Keyboard binds for easier insertions
 root.bind("<quotedbl>", lambda event: type.set(1))
 root.bind("<sterling>", lambda event: type.set(2))
 
 root.bind("<Up>", lambda event: type.set((type.get()-1)%3))
 root.bind("<Down>", lambda event: type.set((type.get()+1)%3))
 
-pos = IntVar()
+pos = IntVar() # Position insertion widgets
 pos.set(0)
 one = Radiobutton(root, variable=pos, value=0, text="1st letter")
 one.grid(row=1, column=3, sticky=N+S+W+E)
@@ -81,14 +72,13 @@ forth.grid(row=4, column=3, sticky=N+S+W+E)
 forth = Radiobutton(root, variable=pos, value=4, text="5th letter")
 forth.grid(row=5, column=3, sticky=N+S+W+E)
 
-
-root.bind('1', lambda event: pos.set(0))
+root.bind('1', lambda event: pos.set(0)) # Keyboard binds for easier insertions
 root.bind('2', lambda event: pos.set(1))
 root.bind('3', lambda event: pos.set(2))
 root.bind('4', lambda event: pos.set(3))
 root.bind('5', lambda event: pos.set(4))
 
-label_list = []
+label_list = [] # Insertion function
 len_labels = 0
 def insert_clue():
     global pos
@@ -98,22 +88,23 @@ def insert_clue():
     global label_list
 
     clues.append((letter.get(), type.get(), pos.get()))
-    placeholder = ""
-    for i in range(5):
+    placeholder = "" # Adding info to list of clues
+
+    for i in range(5): # Showing feedback to user: position
         if i == pos.get():
             placeholder += "⬛"
         else:
             placeholder += "⬜"
 
-    placeholder = f"({letter.get()}) {placeholder}"
+    placeholder = f"({letter.get()}) {placeholder}" # letter feedback
     
-    back = "gray"
+    back = "gray" # type feedback
     if type.get() == 1:
         back = "yellow"
     elif type.get() == 2:
         back = "green"
     
-    label = Label(root, text=placeholder, bg=back)
+    label = Label(root, text=placeholder, bg=back) # Displaying inserted letter
     label.grid(row=1+len_labels%5, column=4+len_labels//5)
     label_list.append(label)
     
@@ -128,10 +119,11 @@ insert.grid(row=5, column=0)
 
 root.bind("<space>", lambda event: insert_clue())
 
-def delete_clue():
+def delete_clue(): # Deletion function, remove last clue
     global clues
     global label_list
     global len_labels
+
     if len_labels != 0:
         label_list[len_labels-1].grid_forget()
         clues.pop(-1)
@@ -142,20 +134,20 @@ delete.grid(row=5, column=1)
 
 root.bind("<minus>", lambda event: delete_clue())
 
-def submit():
+def submit(): # Submit function
     global clues
     global words_list
 
-    words_list = original
+    words_list = original # Resetting list
 
-    words_list = update_list(words_list, clues)
+    words_list = update_list(words_list, clues) # Update list
 
     prob_list = []
     entropy_list = []
     with open("word_file.json", "r") as fp:
         data = json.load(fp)
 
-    if len(words_list) >= 100:
+    if len(words_list) >= 100: # Getting probability and entropy from every word
         for word in words_list:
             prob_list.append(data[word]["probability"])
             entropy_list.append(data[word]["entropy"])
@@ -172,13 +164,12 @@ def submit():
     results.iconbitmap("materials/duh.ico")
     results.title("Results")
 
-
     label =  Label(results, text="RESULTS")
     label.grid(row=0, column=0)
     length = Label(results, text=f"{len(words_list)}/{len(original)} words")
     length.grid(row=0, column=1)
 
-    if len(words_list) == 0:
+    if len(words_list) == 0: # Text based on number of words
         prob_text = "No words possible"
         entropy_text = "No words possible"
     elif len(words_list) == 1:
@@ -192,7 +183,7 @@ def submit():
         entropy_text = f"Most efficient words:\n 1. {entropy_list[0]}\n 2. {entropy_list[1]}\n 3. {entropy_list[2]}"
 
 
-    best_prob = Label(results, text=prob_text)
+    best_prob = Label(results, text=prob_text) # Widget to show results
     best_prob.grid(row=1, column=0)
 
     best_entropy = Label(results, text=entropy_text)
@@ -219,7 +210,7 @@ def submit():
         prob_word.set(prob_list[0])
         entropy_word.set(entropy_list[0])
 
-    prob_drop = OptionMenu(results, prob_word, *prob_list)
+    prob_drop = OptionMenu(results, prob_word, *prob_list) # Widget with the remaining words in a list
     prob_drop.grid(row=4, column=0)
     entropy_drop = OptionMenu(results, entropy_word, *entropy_list)
     entropy_drop.grid(row=4, column=1)
@@ -228,6 +219,7 @@ submit_btn = Button(root, text="Submit", command=submit)
 submit_btn.grid(row=5, column=2, padx=(0, 15))
 
 root.bind("<Return>", lambda event: submit())
+
 
 
 root.mainloop()
